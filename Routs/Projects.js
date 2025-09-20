aa18f088691a526c32430b773a75ae68f5516592
officerkiruha <kirillkovalchukstudy@gmail.com>
2025-09-20 12:05:31 +0300

    add first get and post that get the all information

--- Routs/Projects.js
+++ Routs/Projects.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const fs = require('fs');
+let nextID = 1;
+let Projects = [];
 
 if(!fs.existsSync('Uploads')){
     fs.mkdirSync('Uploads');
@@ -12,3 +14,21 @@ const storage = multer.diskStorage({
         cb(null,'Uploads/');
     }
 });
+
+const upload = multer({storage:storage});
+
+router.get('/',(req,res)=>{
+    res.json(Projects)
+});
+router.post('/',upload.single('myProject'),(req,res)=>{
+    let id = nextID++;
+    let name = req.body.name;
+    let description = req.body.description;
+    let filename = req.file ? req.file.filename:null;
+    let rating = 0;
+    let obj = {id,name,description,filename,rating};
+    Projects[id]=obj;
+    res.json({message:"added"})
+});
+
+s
